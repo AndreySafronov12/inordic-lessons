3aacc8859a677eb82076ce1316e773ca280360b7
Andrey <andrey.mail.12@gmail.com>
2021-02-19 19:33:24 +0300

    Переработана задача с поиском среднего, минимального и максимального значения

--- src/main/java/org/example/l6/Task2.java
+++ src/main/java/org/example/l6/Task2.java
@@ -3,10 +3,29 @@ package org.example.l6;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(3, 5, 3, 6, 10, 8);
+        System.out.print("Введите количество элементов: ");
+        int num = new Scanner(System.in).nextInt();
+
+        List<Integer> list = getArray(num);
+
+        getAvrgMinMax(list);
+    }
+
+    public static List<Integer> getArray (int a) {
+        List<Integer> list = new ArrayList<> ();
+        var scanner = new Scanner(System.in);
+        for (int i = 0; i < a; i++) {
+            System.out.print("Введите [" + i + "] элемент: ");
+            list.add(scanner.nextInt());
+        }
+        return list;
+    }
+
+    public static void getAvrgMinMax (List<Integer> list) {
         int sum = 0;
         int min = list.get(0);
         int max = list.get(0);
@@ -16,10 +35,9 @@ public class Task2 {
             if (list.get(i) < min) min = list.get(i);
             if (list.get(i) > max) max = list.get(i);
         }
-        System.out.println(sum / list.size());
-        System.out.println(min);
-        System.out.println(max);
 
-
+        System.out.println("Среднее арифметическое: " + (double) sum / (double) list.size());
+        System.out.println("Минимальное значение: " + min);
+        System.out.println("Максимальное значение: " + max);
     }
 }
