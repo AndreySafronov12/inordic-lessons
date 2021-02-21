3f711dccf677abc5eb6a2b3768e4bf63dc66bbce
Andrey <andrey.mail.12@gmail.com>
2021-02-21 18:34:41 +0300

    Добавлены методы сортировки и исключения повторяющихся элементов

--- src/main/java/org/example/l7/uniqueArray.java
+++ src/main/java/org/example/l7/uniqueArray.java
@@ -10,30 +10,12 @@ public class uniqueArray {
         int num = new Scanner(System.in).nextInt();
 
         List<Integer> list = inputArray(num);
-        List<Integer> uniqueList = new ArrayList<>();
-        List<Integer> sortList = new ArrayList<>();
 
+        System.out.print("Уникальные элементы списка: ");
+        System.out.println(getUniqueList(list));
 
-        for (int i = 0; i < list.size(); i++) {
-            int temp = 0;
-            for (int j = 0; j < uniqueList.size(); j++) {
-                if (list.get(i) == uniqueList.get(j)) temp++;
-            }
-            if (temp == 0) uniqueList.add(list.get(i));
-
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            int temp = list.get(0);
-            for (int j = i; j < uniqueList.size(); j++) {
-                if (list.get(i) == uniqueList.get(j)) temp++;
-            }
-            if (temp == 0) uniqueList.add(list.get(i));
-
-        }
-
-        System.out.println(uniqueList);
-
+        System.out.print("Отсортированный список: ");
+        System.out.println(getSortList(list));
     }
 
     public static List<Integer> inputArray(int a) {
@@ -42,6 +24,31 @@ public class uniqueArray {
         for (int i = 0; i < a; i++) {
             System.out.print("Введите [" + i + "] элемент: ");
             list.add(scanner.nextInt());
+        }
+        return list;
+    }
+
+    public static List<Integer> getUniqueList (List<Integer> list) {
+        List<Integer> uniqueList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            int temp = 0;
+            for (int j = 0; j < uniqueList.size(); j++) {
+                if (list.get(i) == uniqueList.get(j)) temp++;
+            }
+            if (temp == 0) uniqueList.add(list.get(i));
+        }
+        return uniqueList;
+    }
+
+    public static List<Integer> getSortList (List<Integer> list) {
+        for (int i = 0; i < (list.size() - 1); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(i)) {
+                    int swap = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, swap);
+                }
+            }
         }
         return list;
     }
