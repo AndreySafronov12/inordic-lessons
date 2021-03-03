982647fc79e1f2f7322d61e19038c2b2e76a769f
Andrey <andrey.mail.12@gmail.com>
2021-03-04 01:38:06 +0300

    Переписаны исключения

--- src/main/java/org/example/l7/zoo/exceptions/EatingException.java
+++ src/main/java/org/example/l7/zoo/exceptions/EatingException.java
@@ -4,4 +4,20 @@ public class EatingException extends RuntimeException {
     public EatingException(String message) {
         super(message);
     }
+
+    public EatingException() {
+        super();
+    }
+
+    public EatingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EatingException(Throwable cause) {
+        super(cause);
+    }
+
+    public EatingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
